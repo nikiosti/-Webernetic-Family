a719514d45ba311385a05bbd52a25255b9a85618
nikiosti <yaostin@proton.me>
2024-06-17 13:06:31 +0300

    change: className open modal in js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const button = document.getElementById('modal__button')
+const button = document.getElementById('modal-button')
 const modal = document.getElementById('modal')
+const closeButton = document.getElementById('close-button')
 
 button.addEventListener('click', () => {
   modal.style.display = 'block'
 })
 
-const closeButton = document.getElementById('closeButton')
 closeButton.addEventListener('click', () => {
   modal.style.display = 'none'
 })
